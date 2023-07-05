a9ebb610bdabb4ba35e7c9353887410acb3595a3
josh <josh@bitperfect.at>
2023-07-05 13:25:36 +0200

    Investigate deploy problem

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+declare var __dirname: string;
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                popover: resolve(__dirname, "popover.html"),
+                background: resolve(__dirname, "background.html"),
+            },
+        },
+    },
+});
