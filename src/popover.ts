c75655913c51bf48ffab22783abb29dfdda8958a
josh <josh@bitperfect.at>
2023-07-21 20:24:02 +0200

    Fix popover removal

--- src/popover.ts
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import React from "react";
-import "./main.scss";
-import { Popover } from "./components/popover/Popover.tsx";
-
-const root = ReactDOM.createRoot(<HTMLElement>document.querySelector("#app"));
-root.render(React.createElement(Popover));
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                popover: resolve(__dirname, "popover.html"),
                 background: resolve(__dirname, "background.html"),
             },
         },
