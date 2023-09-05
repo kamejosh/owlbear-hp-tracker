f40dc64438127cb54312191bdef2341fbb8ca0e7
Joshua Hercher <kamejosh@users.noreply.github.com>
2023-09-05 07:41:09 +0200

    Update variables.ts
    This is a piece that you really have to change, otherwise both extensions occupy the same metadata space.

--- src/helper/variables.ts
+++ src/helper/variables.ts
@@ -1,4 +1,4 @@
-export const ID = "com.bitperfect-software.hp-tracker";
+export const ID = "com.treyumsey.shield-and-armor-tracker";
 export const sceneMetadata = `${ID}/metadata`;
 export const characterMetadata = `${ID}/data`;
 export const infoMetadata = `${ID}/text`;
