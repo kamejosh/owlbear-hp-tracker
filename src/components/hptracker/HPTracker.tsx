315b625ba8b8282829307128edd89f6fd09d70af
josh <josh@bitperfect.at>
2023-12-11 14:40:56 +0100

    Add patreon link

--- src/components/hptracker/HPTracker.tsx
+++ src/components/hptracker/HPTracker.tsx
@@ -189,6 +189,15 @@ const Content = () => {
         ) : (
             <div className={"hp-tracker"}>
                 <div className={"help-buttons"}>
+                    <a href={"https://www.patreon.com/TTRPGAPI"} className={"patreon-button"} target={"_blank"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 436 476">
+                            <path
+                                data-fill="1"
+                                d="M436 143c-.084-60.778-47.57-110.591-103.285-128.565C263.528-7.884 172.279-4.649 106.214 26.424 26.142 64.089.988 146.596.051 228.883c-.77 67.653 6.004 245.841 106.83 247.11 74.917.948 86.072-95.279 120.737-141.623 24.662-32.972 56.417-42.285 95.507-51.929C390.309 265.865 436.097 213.011 436 143Z"
+                                fill={"#dddddd"}
+                            ></path>
+                        </svg>
+                    </a>
                     {playerContext.role == "GM" ? (
                         <button className={"settings-button"} onClick={async () => await OBR.modal.open(settingsModal)}>
                             ⛭
