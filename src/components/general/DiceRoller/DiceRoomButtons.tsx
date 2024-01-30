5753d158d736903733ae00bb027d763b35891c56
josh <josh@bitperfect.at>
2024-01-30 11:06:01 +0100

    Add custom dice buttons

--- src/components/general/DiceRoller/DiceRoomButtons.tsx
+++ src/components/general/DiceRoller/DiceRoomButtons.tsx
@@ -9,6 +9,7 @@ import { diceToRoll } from "../../../helper/diceHelper.ts";
 import { dddiceRollToRollLog } from "../../../helper/helpers.ts";
 import { useComponentContext } from "../../../context/ComponentContext.tsx";
 import tippy from "tippy.js";
+import { RollLogSvg } from "../../svgs/RollLogSvg.tsx";
 
 type DiceRoomButtonsProps = {
     open: boolean;
@@ -26,7 +27,10 @@ const CustomDiceButton = (props: CustomDiceButtonProps) => {
     const { buttons, setButtons } = useDiceButtonsContext();
     const { component } = useComponentContext();
     const [hover, setHover] = useState<boolean>(false);
+    const [addCustom, setAddCustom] = useState<boolean>(false);
+    const [validCustom, setValidCustom] = useState<boolean>(false);
     const buttonRef = useRef<HTMLButtonElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         if (buttonRef.current) {
@@ -55,7 +59,7 @@ const CustomDiceButton = (props: CustomDiceButtonProps) => {
                             } else {
                                 if (die.value) {
                                     return (
-                                        <span key={index}>
+                                        <span key={index} className={"modifier"}>
                                             {Intl.NumberFormat("en-US", { signDisplay: "always" }).format(die.value)}
                                         </span>
                                     );
@@ -94,16 +98,17 @@ const CustomDiceButton = (props: CustomDiceButtonProps) => {
     };
 
     return (
-        <div className={"custom-dice-wrapper"} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+        <div
+            className={`custom-dice-wrapper ${props.dice ? "has-dice" : ""}`}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+        >
             <button
                 ref={buttonRef}
                 className={`button custom-dice dice-${props.button}`}
                 onClick={async (e) => {
                     if (!props.dice && buttons.hasOwnProperty(props.button.toString())) {
-                        const newButton = {
-                            [props.button]: "1d4 2d6",
-                        };
-                        setButtons(newButton);
+                        setAddCustom(true);
                     } else if (props.dice) {
                         await roll(e.currentTarget);
                     }
@@ -111,6 +116,58 @@ const CustomDiceButton = (props: CustomDiceButtonProps) => {
             >
                 {props.dice ? getDicePreview() : <AddSvg />}
             </button>
+            {addCustom ? (
+                <div className={"add-custom-dice"}>
+                    <input
+                        disabled={!theme}
+                        ref={inputRef}
+                        type={"text"}
+                        onChange={(e) => {
+                            const value = e.currentTarget.value;
+                            try {
+                                if (theme) {
+                                    const parsed = parseRollEquation(value, theme);
+                                    if (parsed) {
+                                        setValidCustom(true);
+                                        inputRef.current?.classList.remove("error");
+                                        inputRef.current?.classList.add("success");
+                                    }
+                                }
+                            } catch {
+                                setValidCustom(false);
+                                inputRef.current?.classList.remove("success");
+                                inputRef.current?.classList.add("error");
+                            }
+                        }}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter" && validCustom) {
+                                const newButton = {
+                                    [props.button]: e.currentTarget.value,
+                                };
+                                setButtons(newButton);
+                                setAddCustom(false);
+                                setValidCustom(false);
+                            }
+                        }}
+                    />
+                    <button
+                        className={"save-custom-dice"}
+                        disabled={!validCustom}
+                        onClick={() => {
+                            if (inputRef.current) {
+                                const newButton = {
+                                    [props.button]: inputRef.current.value,
+                                };
+                                setButtons(newButton);
+                                setAddCustom(false);
+                                setValidCustom(false);
+                            }
+                        }}
+                    >
+                        √
+                    </button>
+                </div>
+            ) : null}
             {props.dice ? (
                 <button
                     className={`remove-dice ${hover ? "hover" : ""}`}
@@ -132,6 +189,15 @@ const CustomDiceButton = (props: CustomDiceButtonProps) => {
 
 export const DiceRoomButtons = (props: DiceRoomButtonsProps) => {
     const { buttons } = useDiceButtonsContext();
+    const logButtonRef = useRef<HTMLButtonElement>(null);
+
+    useEffect(() => {
+        if (logButtonRef.current) {
+            tippy(logButtonRef.current, {
+                content: "open roll log",
+            });
+        }
+    }, [logButtonRef]);
 
     return (
         <div className={"dice-room-buttons"}>
@@ -139,6 +205,7 @@ export const DiceRoomButtons = (props: DiceRoomButtonsProps) => {
                 return <CustomDiceButton key={index} button={index + 1} dice={value} />;
             })}
             <button
+                ref={logButtonRef}
                 className={`open-dice-tray button icon ${props.open ? "open" : "closed"}`}
                 onClick={(e) => {
                     props.setOpen(!props.open);
@@ -146,7 +213,7 @@ export const DiceRoomButtons = (props: DiceRoomButtonsProps) => {
                     e.currentTarget.blur();
                 }}
             >
-                <DiceSvg />
+                <RollLogSvg />
             </button>
         </div>
     );
